89f9124545c78358e46368c1d4e62a1b2d120bd5
Ryan <ryanwstowe@gmail.com>
2019-09-05 13:14:27 -0400

    stopping point

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,8 @@
+import React, {Fragment} from 'react';
+import Spinner from './spinner.gif'
+
+const Spinner = () => {
+  <Fragment>
+    <img src={Spinner} alt="Loading..." style={{width: '200px' margin: 'auto' display: 'block;'}}/>
+  </Fragment>
+}
