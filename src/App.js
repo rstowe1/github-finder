6b1a386fe785e6213fcffe0dabc4fa85b1a38a2f
Ryan <ryanwstowe@gmail.com>
2019-09-14 17:22:36 -0400

    mostly completed with context api

--- src/App.js
+++ src/App.js
@@ -1,103 +1,59 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
 
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
 import './App.css';
 import Search from "./components/users/Search";
 
 
-class App extends Component {
-  state = {
-    users: [],
-    user: [],
-    loading: false,
-    alert: null
-  };
+const App = () => {
 
-  // SEARCH GITHUB USERS
-  searchUsers = async text => {
-    this.setState({loading: true});
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({users: res.data.items, loading: false})
-  };
-
-  // GET A SINGLE GITHUB USER
-
-  getUser = async (username) => {
-    this.setState({loading: true});
-
-    const res = await axios.get(`https://api.github.com/users${username}&${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({user: res.data, loading: false})
-  }
-
-  // CLEAR USERS FROM STATE
-  clearUsers = () => this.setState({users: [], loading: false});
-
-  //SET ALERT
-  setAlert = (msg, type) => {
-    this.setState({alert: {msg: msg, type: type}});
-    setTimeout(() => this.setState({alert: null}), 5000)
-  };
-
-  render(props) {
-    const {users, user, loading} = this.state;
-    return (
-      <Router>
-        <div
-          className="App">
-          <Navbar
-            title='Github Finder'
-            icon="fab fa-github"/>
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
           <div
-            className="container">
-            <Alert
-              alert={this.state.alert}/>
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}
-                    />
-                    <Users
-                      loading={loading}
-                      users={users}/>
-                  </Fragment>
-                )}/>
-              <Route
-                exact
-                path='/about'
-                component={About}/>
-              <Route
-                exact
-                path='/user:login'
-                render={props => (
-                  <User
-                    {...props}
-                    getUser={this.getUser}
-                    user={user}
-                    loading={loading}/>
+            className="App">
+            <Navbar
+              title='Github Finder'
+              icon="fab fa-github"/>
+            <div
+              className="container">
+              <Alert/>
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={props => (
+                    <Fragment>
+                      <Search/>
+                      <Users/>
+                    </Fragment>
+                  )}/>
+                <Route
+                  exact
+                  path='/about'
+                  component={About}/>
+                <Route
+                  exact
+                  path='/user/:login'
+                  component={User}/>
                 )}
-              />
-            </Switch>
+                />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
-    );
-  }
-}
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -6,13 +6,18 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
 
   return (
     <div className='card text-center'>
-      <img src={avatar_url}
-           alt=""
-           className='round-img'
-           style={{width: '60px'}}/>
+      <img
+        src={avatar_url}
+        alt=""
+        className='round-img'
+        style={{width: '60px'}}/>
       <h3>{login}</h3>
       <div>
-        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link>
+        <Link
+          to={`/user/${login}`}
+          className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
       </div>
     </div>
   )
